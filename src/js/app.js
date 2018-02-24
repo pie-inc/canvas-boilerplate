ff827f3628040c171ae540537d337fadd18109b6
J Lopes <offeringofpie@gmail.com>
2018-02-24 17:27:20 +0100

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import Game from './classes/Game';
+const canvas = document.querySelector('canvas');
+const game = new Game(canvas);
+
+game.init();
---
+++ src/js/classes/Game.js
@@ -0,0 +1,28 @@
+import {Observable, Subject} from "rxjs/Observable";
+import 'rxjs/add/observable/fromEvent';
+
+export default class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.context = canvas.getContext('2d');
+  }
+
+  init() {
+    this.resizeCanvas();
+    this.fillArea('rgba(0,0,255,.8)',0,0,100,100);
+    this.fillArea('rgba(0,255,0,.5)',50,50,100,100);
+  }
+
+  resizeCanvas(width = window.innerWidth, height = window.innerHeight) {
+    this.canvas.width = width;
+    this.canvas.height = height;
+  }
+
+  fillArea(
+    color = "green",
+    x = 0, y = 0,
+    width = window.innerWidth, height = window.innerHeight) {
+    this.context.fillStyle = color;
+    this.context.fillRect(x,y,width,height);
+  }
+}
